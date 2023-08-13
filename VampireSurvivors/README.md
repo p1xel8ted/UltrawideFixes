4d49046b0a0b61acc6856928d37ce6de49a139f5
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-08-13 14:53:50 +1000

    Vampire Survivors 0.0.4

--- VampireSurvivors/README.md
+++ VampireSurvivors/README.md
@@ -18,17 +18,13 @@ An ultra-wide fix for Vampire Survivors that removes pillar-boxing. Uses Melon L
 
 - Extract the contents of the release zip into the game directory. (e.g. "**steamapps\common\Vampire Survivors**" for Steam).
 - Run 'MelonLoader.Installer.exe'.
-- Choose the Settings tab and enable 'Show Alpha Pre-Releases.'
+- Click 'Select' next to Unity Game and navigate to the game directory, selecting 'Vampire Survivors.exe'.
 - Choose the Automated tab and select version 0.6.1 from the drop-down.
 - Leave Game Arch on Auto.
 - Click Install
 
-## Known Issues
-
-- **The 'challenge' levels have issues where the ground tileset stops rendering outside of 16:9. Those levels have been implemented differently compared to the others. No fix at this stage.**
-- Enemies pop in where the black bars would normally be.
-- Not a mod issue, but the game appears zoomed in compared to the original engine.
-
 ## Notes
 
+- I've removed the Expand UI option.
 - The game's first run will take a little longer while Melon Loader works its magic, but this only happens once (or on game updates).
+- This may break on updates.
--- VampireSurvivors/VampireSurvivors/VampireSurvivors.cs
+++ VampireSurvivors/VampireSurvivors/VampireSurvivors.cs
@@ -1,164 +1,100 @@
-﻿using Il2CppInterop.Runtime.InteropTypes.Arrays;
-using Il2CppVampireSurvivors.Data;
+﻿using Il2Cpp;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using Il2CppVampireSurvivors.UI;
 using MelonLoader;
 using UnityEngine;
 using UnityEngine.UI;
 
 [assembly:
-    MelonInfo(typeof(VampireSurvivors.VampireSurvivors), "Vampire Survivors UltraWide Fixes", "0.0.3", "p1xel8ted")]
-[assembly: MelonGame("poncle", "VampireSurvivors")]
+    MelonInfo(typeof(VampireSurvivors.VampireSurvivors), "Vampire Survivors UltraWide Fixes", "0.0.4", "p1xel8ted")]
+[assembly: MelonGame("poncle", "Vampire Survivors")]
 
 namespace VampireSurvivors;
 
-public partial class VampireSurvivors : MelonMod
+public class VampireSurvivors : MelonMod
 {
-    private MelonPreferences_Category _modCategory;
-    private static MelonPreferences_Entry<bool> _expandUI;
-    private static MelonPreferences_Entry<bool> _blackChallenge;
-    private GameObject _map;
-    private GameObject _left;
-    private Il2CppArrayBase<AspectMask> _aspectMasks;
+    private Il2CppArrayBase<ViewportCrop> _viewportCrop;
     private Il2CppArrayBase<Camera> _cameras;
     private Il2CppArrayBase<AspectRatioFitter> _aspectRatioFitters;
-    private static StageType CurrentType { get; set; } = StageType.TEST;
+    private Il2CppArrayBase<AspectMask> _aspectMasks;
 
     public override void OnInitializeMelon()
     {
-        _modCategory = MelonPreferences.CreateCategory("Vampire Survivors UltraWide Fixes");
-        _expandUI = _modCategory.CreateEntry("Expand UI", false, description:"Expand the UI to fill the screen. May/probably will break some UI elements (minor).");
-        _blackChallenge = _modCategory.CreateEntry("Leave Black Bars In-Place For Challenge Levels", true, description:"The challenge levels ground tile-set stops rendering outside of 16:9. This option will leave the black bars in place for those levels.");
+        base.OnInitializeMelon();
+
+        _cameras = Resources.FindObjectsOfTypeAll<Camera>();
+        _viewportCrop = Resources.FindObjectsOfTypeAll<ViewportCrop>();
+        _aspectRatioFitters = Resources.FindObjectsOfTypeAll<AspectRatioFitter>();
+        _aspectMasks = Resources.FindObjectsOfTypeAll<AspectMask>();
+
+        MelonLogger.Warning($"OnInitializeMelon: Found {_cameras.Length} Cameras");
+        MelonLogger.Warning($"OnInitializeMelon: Found {_viewportCrop.Length} Viewport Crops");
+        MelonLogger.Warning($"OnInitializeMelon: Found {_aspectRatioFitters.Length} Aspect Ratio Fitters");
+        MelonLogger.Warning($"OnInitializeMelon: Found {_aspectMasks.Length} Aspect Masks");
+
         MelonLogger.Msg("Vampire Survivors UltraWide Fixes Loaded");
-        MelonPreferences.Save();
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
 
-        if (_expandUI.Value && _map != null && _left != null)
+        foreach (var aspectMask in _aspectMasks.Where(a => a != null))
         {
-            _map.transform.SetParent(_left.transform);
-            _map.transform.localPosition = new Vector3(0, -1138, 0);
-            
-        }
-        foreach (var a in _aspectRatioFitters)
-        {
-            if (_blackChallenge.Value && CurrentType is StageType.MOLISE or StageType.GREENACRES)
-            {
-                a.aspectRatio = 1.6f;
-            }
-            else
-            {
-                a.aspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
-            }
-        }
-
-        foreach (var aspectMask in _aspectMasks.Where(a=>a!=null))
-        {
-            if (_blackChallenge.Value)
-            {
-                if (CurrentType is StageType.MOLISE or StageType.GREENACRES)
-                {
-                    aspectMask.gameObject.SetActive(true);
-                    return;
-                }
-            }
-
             if (aspectMask.gameObject.activeSelf)
             {
                 aspectMask.gameObject.SetActive(false);
             }
         }
-        
-        
+
+        foreach (var viewportCrop in _viewportCrop.Where(a => a != null))
+        {
+            viewportCrop._currentAspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
+            viewportCrop._referenceResolution = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+            viewportCrop.ScreenRes = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+        }
+
     }
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
         base.OnSceneWasLoaded(buildIndex, sceneName);
-        
+
         _cameras = Resources.FindObjectsOfTypeAll<Camera>();
+        _viewportCrop = Resources.FindObjectsOfTypeAll<ViewportCrop>();
+        _aspectRatioFitters = Resources.FindObjectsOfTypeAll<AspectRatioFitter>();
         _aspectMasks = Resources.FindObjectsOfTypeAll<AspectMask>();
         
-        foreach (var aspectMask in _aspectMasks.Where(a=>a!=null))
-        {   
-            if (_blackChallenge.Value)
-            {
-                if (CurrentType is StageType.MOLISE or StageType.GREENACRES)
-                {
-                    aspectMask.gameObject.SetActive(true);
-                    return;
-                }
-            }
+        MelonLogger.Warning($"OnSceneWasLoaded: Found {_cameras.Length} Cameras");
+        MelonLogger.Warning($"OnSceneWasLoaded: Found {_viewportCrop.Length} Viewport Crop");
+        MelonLogger.Warning($"OnSceneWasLoaded: Found {_aspectRatioFitters.Length} Aspect Ratio Fitters");
+        MelonLogger.Warning($"OnSceneWasLoaded: Found {_aspectMasks.Length} Aspect Masks");
 
+        foreach (var aspectMask in _aspectMasks.Where(a => a != null))
+        {
             if (aspectMask.gameObject.activeSelf)
             {
                 aspectMask.gameObject.SetActive(false);
             }
         }
-        
-        if (_expandUI.Value )
-        {
-            _aspectRatioFitters = Resources.FindObjectsOfTypeAll<AspectRatioFitter>();
-            foreach (var a in _aspectRatioFitters)
-            {
-                if (_blackChallenge.Value && CurrentType is StageType.MOLISE or StageType.GREENACRES)
-                {
-                    a.aspectRatio = 1.6f;
-                }
-                else
-                {
-                    a.aspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
-                }
-            }
-        
-            _left = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - Paused/Left");
-            _map = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - Paused/MapGroup");
-        
-            var charSelection = GameObject.Find("UI/Canvas - App/Safe Area/View - CharacterSelection/Panel");
-            if (charSelection != null)
-            {
-                var confirmButton =
-                    GameObject.Find("UI/Canvas - App/Safe Area/View - CharacterSelection/ConfirmButton");
-                var buyButton = GameObject.Find("UI/Canvas - App/Safe Area/View - CharacterSelection/BuyButton");
-                var startButton =
-                    GameObject.Find("UI/Canvas - App/Safe Area/View - CharacterSelection/StartButton");
-                GameObject[] buttons = {confirmButton, buyButton, startButton};
 
-                foreach (var button in buttons.Where(a=>a!=null))
-                {
-                    button.transform.SetParent(charSelection.transform);
-                    button.transform.localPosition = new Vector3(693.5758f, 81.4038f, 0);
-                }
-            }
-        
-            var selectPanel = GameObject.Find("UI/Canvas - App/Safe Area/View - StageSelection/Panel");
-            if (selectPanel != null)
-            {
-                var stageSelectButton =
-                    GameObject.Find("UI/Canvas - App/Safe Area/View - StageSelection/SelectButton");
-                var stageConfirmButton =
-                    GameObject.Find("UI/Canvas - App/Safe Area/View - StageSelection/ConfirmButton");
-                var stageStartButton =
-                    GameObject.Find("UI/Canvas - App/Safe Area/View - StageSelection/StartButton");
-                GameObject[] stageButtons = {stageSelectButton, stageConfirmButton, stageStartButton};
-                
-                foreach (var button in stageButtons.Where(a=>a!=null))
-                {
-                    button.transform.SetParent(selectPanel.transform);
-                    button.transform.localPosition = new Vector3(693.5758f, 81.4038f, 0);
-                }
-                
-                var musicPanel = GameObject.Find("UI/Canvas - App/Safe Area/View - StageSelection/Music Selection");
-                if (musicPanel != null)
-                {
-                    musicPanel.transform.SetParent(selectPanel.transform);
-                    musicPanel.transform.localPosition = new Vector3(693.5758f, 375.1111f, 0);
-                }
-               
-            }
+        foreach (var viewportCrop in _viewportCrop.Where(a => a != null))
+        {
+            viewportCrop._currentAspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
+            viewportCrop._referenceResolution = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+            viewportCrop.ScreenRes = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+        }
+
+        var backgroundFade = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - Paused/Fade");
+        if (backgroundFade != null)
+        {
+            backgroundFade.transform.localScale = new Vector3(5f, 1, 1);
+        }
+
+        var backFade = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - ArcanaMainSelection/BlackFader");
+        if (backFade != null)
+        {
+            backFade.transform.localScale = new Vector3(5f, 1, 1);
         }
     }
-    
 }
