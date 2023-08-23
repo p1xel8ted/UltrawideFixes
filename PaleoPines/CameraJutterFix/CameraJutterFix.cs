0020584cdae029e764b9cc1a2fec1c840d50f62c
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-08-23 19:16:44 +1000

    Paleo Pines Camera Jutter Fix

---
+++ PaleoPines/CameraJutterFix/CameraJutterFix.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(CameraJutterFix.CameraJutterFix), "Camera Jutter Fix", "0.1.0", "p1xel8ted")]
+
+namespace CameraJutterFix;
+
+public class CameraJutterFix : MelonMod
+{
+    public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+    {
+        base.OnSceneWasInitialized(buildIndex, sceneName);
+        LoggerInstance.Warning("Original Time.fixedDeltaTime: " + Time.fixedDeltaTime);
+        Time.fixedDeltaTime = 1 / (float) Screen.currentResolution.refreshRate;
+        LoggerInstance.Warning("New Time.fixedDeltaTime: " + Time.fixedDeltaTime);
+    }
+}
