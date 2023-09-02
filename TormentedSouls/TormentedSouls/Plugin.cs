3c25dd6a09dfa68942cbb2b72f9d61cd2844e1e9
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-09-02 21:28:20 +1000

    Tormented Souls

--- TormentedSouls/TormentedSouls/Plugin.cs
+++ TormentedSouls/TormentedSouls/Plugin.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace TormentedSouls;
 
@@ -17,21 +20,60 @@ public class Plugin : BaseUnityPlugin
     private const string LogosInit = "LogosInit";
     private const string MenuInit = "MenuInit";
     private const string Background = "Background";
+    private const int ReferenceResolutionHeight = 2160;
     private static ManualLogSource Log { get; set; }
+    private static ConfigEntry<float> ConfigScale { get; set; }
+    private static GameObject CinematicBlackBarsTop { get; set; }
+    private static GameObject CinematicBlackBarsBottom { get; set; }
+    private static List<CanvasScaler> CanvasScalers { get; } = new();
+
+    private static float GetNewScale()
+    {
+        const float referenceHeight = 2160f;
+        var displayHeight = Display.main.systemHeight;
+        var scale = 1f / (referenceHeight / displayHeight);
+        return scale;
+    }
 
     private void Awake()
     {
+        ConfigScale = Config.Bind("General", "Scale", GetNewScale(),
+            new ConfigDescription("Scale of the UI. Adjust as necessary to fit. The default value should OK.",
+                new AcceptableValueRange<float>(0.1f, 2f), new ConfigurationManagerAttributes {Order = 1}));
+        ConfigScale.SettingChanged += (_, _) =>
+        {
+            foreach (var scaler in CanvasScalers)
+            {
+                scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+                scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Shrink;
+                scaler.scaleFactor = ConfigScale.Value;
+            }
+        };
         Log = Logger;
         SceneManager.sceneLoaded += OnSceneLoaded;
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
         Log.LogInfo($"Plugin {PluginGuid} is loaded!");
     }
 
-    private static GameObject CinematicBlackBarsTop { get; set; }
-    private static GameObject CinematicBlackBarsBottom { get; set; }
-
     private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
+        var canvasScalers = Resources.FindObjectsOfTypeAll<CanvasScaler>();
+        foreach (var scaler in canvasScalers)
+        {
+            if (!Mathf.Approximately(scaler.referenceResolution.y, ReferenceResolutionHeight)) continue;
+
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+            scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Shrink;
+            scaler.scaleFactor = GetNewScale();
+            CanvasScalers.Add(scaler);
+        }
+
+        var videoPlayers = Resources.FindObjectsOfTypeAll<VideoPlayer>();
+        foreach (var vp in videoPlayers)
+        {
+            vp.aspectRatio = VideoAspectRatio.FitVertically;
+        }
+
         CinematicBlackBarsTop ??= GameObject.Find("[LoadingCurtainsManager]/Canvas/CinematicBlackBars/TopBar");
         if (CinematicBlackBarsTop != null)
         {
