5a068631fbe84d1e1ad5223f2cdc582154c27992
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-09-03 14:13:39 +1000

    Update UI canvas scalers to shrink and scale based on a new reference resolution height.

--- TormentedSouls/TormentedSouls/Plugin.cs
+++ TormentedSouls/TormentedSouls/Plugin.cs
@@ -6,7 +6,6 @@ using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -22,31 +21,35 @@ public class Plugin : BaseUnityPlugin
     private const string LogosInit = "LogosInit";
     private const string MenuInit = "MenuInit";
     private const string Background = "Background";
-    private const int ReferenceResolutionHeight = 2160;
+    private const int UiReferenceResolutionHeight = 2160;
+    private const int DeathReferenceResolutionHeight = 1080;
     private static ManualLogSource Log { get; set; }
     private static ConfigEntry<float> ConfigScale { get; set; }
-    private static GameObject CinematicBlackBarsTop { get; set; }
-    private static GameObject CinematicBlackBarsBottom { get; set; }
-    private static List<CanvasScaler> CanvasScalers { get; } = new();
-    
+    private static List<CanvasScaler> UiCanvasScalers { get; } = new();
     internal static int MaxRefresh => Screen.resolutions.Max(a => a.refreshRate);
 
-    private static float GetNewScale()
+    private static float GetNewScale(float reference)
     {
-        const float referenceHeight = 2160f;
         var displayHeight = Display.main.systemHeight;
-        var scale = 1f / (referenceHeight / displayHeight);
+        var scale = 1f / (reference / displayHeight);
         return scale;
+    }
+    
+    private static void UpdateCanvasScaler(CanvasScaler scaler, float referenceResolutionHeight)
+    {
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Shrink;
+        scaler.scaleFactor = GetNewScale(referenceResolutionHeight);
     }
 
     private void Awake()
     {
-        ConfigScale = Config.Bind("General", "Scale", GetNewScale(),
+        ConfigScale = Config.Bind("General", "Scale", GetNewScale(UiReferenceResolutionHeight),
             new ConfigDescription("Scale of the UI. Adjust as necessary to fit. The default value should OK.",
                 new AcceptableValueRange<float>(0.1f, 2f), new ConfigurationManagerAttributes {Order = 1}));
         ConfigScale.SettingChanged += (_, _) =>
         {
-            foreach (var scaler in CanvasScalers)
+            foreach (var scaler in UiCanvasScalers)
             {
                 scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
                 scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Shrink;
@@ -65,33 +68,48 @@ public class Plugin : BaseUnityPlugin
         Time.fixedDeltaTime = 1f / 120f;
         Application.targetFrameRate = MaxRefresh;
         
+        string[] metaPaths =
+        {
+            "[LoadingCurtainsManager]/Canvas/CinematicBlackBars/BottomBar",
+            "[LoadingCurtainsManager]/Canvas/CinematicBlackBars/TopBar",
+            "FeedbackFrom/MetadataCollectors",
+            "FeedbackFrom/MetadataCollectors/GraphicsInfoCollector",
+            "FeedbackFrom/MetadataCollectors/PlayerInfoCollector",
+            "FeedbackFrom/MetadataCollectors/SystemInfoCollector",
+            "FeedbackFrom/MetadataCollectors/DebugLogCollector",
+            "MainCanvas/OptionsMenuVIew/Panel/LegalLines",
+            "MainCanvas/OptionsMenuVIew/Panel/version"
+        };
+
+        foreach (var path in metaPaths)
+        {
+            var meta = GameObject.Find(path);
+            if (meta != null)
+            {
+                meta.SetActive(false);
+            }
+        }
+
+
         var canvasScalers = Resources.FindObjectsOfTypeAll<CanvasScaler>();
         foreach (var scaler in canvasScalers)
         {
-            if (!Mathf.Approximately(scaler.referenceResolution.y, ReferenceResolutionHeight)) continue;
-
-            scaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
-            scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Shrink;
-            scaler.scaleFactor = GetNewScale();
-            CanvasScalers.Add(scaler);
+            var refHeight = scaler.referenceResolution.y;
+            if (Mathf.Approximately(refHeight, UiReferenceResolutionHeight))
+            {
+                UpdateCanvasScaler(scaler, UiReferenceResolutionHeight);
+                UiCanvasScalers.Add(scaler);
+            }
+            else if (Mathf.Approximately(refHeight, DeathReferenceResolutionHeight))
+            {
+                UpdateCanvasScaler(scaler, DeathReferenceResolutionHeight);
+            }
         }
 
         var videoPlayers = Resources.FindObjectsOfTypeAll<VideoPlayer>();
         foreach (var vp in videoPlayers)
         {
             vp.aspectRatio = VideoAspectRatio.FitVertically;
-        }
-
-        CinematicBlackBarsTop ??= GameObject.Find("[LoadingCurtainsManager]/Canvas/CinematicBlackBars/TopBar");
-        if (CinematicBlackBarsTop != null)
-        {
-            CinematicBlackBarsTop.gameObject.SetActive(false);
-        }
-
-        CinematicBlackBarsBottom ??= GameObject.Find("[LoadingCurtainsManager]/Canvas/CinematicBlackBars/BottomBar");
-        if (CinematicBlackBarsBottom != null)
-        {
-            CinematicBlackBarsBottom.gameObject.SetActive(false);
         }
 
         var backgrounds = Resources.FindObjectsOfTypeAll<Image>();
