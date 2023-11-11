cec5f98d29c3bb27c3e0f569403abb80a84bff51
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-11-11 18:27:22 +1100

    0.1.1

--- Cuisineer/Cuisineer/Patches.cs
+++ Cuisineer/Cuisineer/Patches.cs
@@ -7,23 +7,37 @@ namespace Cuisineer;
 [Harmony]
 public static class Patches
 {
-   
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.InsertHelper))]
+    public static void Inventory_SetSlotData(ref ItemInstance item)
+    {
+        if (!Plugin.IncreaseStackSize.Value || item == null) return;
+        item.ItemSO.m_MaxStack = 999;
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(UI_Cutscene), nameof(UI_Cutscene.RefreshText))]
+    public static void UI_Cutscene_RefreshText(ref UI_Cutscene __instance)
+    {
+        if (!Plugin.InstantText.Value || __instance == null) return;
+        __instance.Skip();
+    }
+    
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(UI_GameplayOptions), nameof(UI_GameplayOptions.Setup))]
     public static void UI_GameplayOptions_Setup()
     {
-        Plugin.Logger.LogInfo("UI_GameplayOptions.Setup");
-
         var myResData = new ResolutionData
         {
             m_Height = Display._mainDisplay.systemHeight,
             m_Width = Display._mainDisplay.systemWidth
         };
         var resDatas = UI_GameplayOptions.ResolutionDatas.ToList();
-        if (!resDatas.Exists(a=>a.m_Height == Display._mainDisplay.systemHeight && a.m_Width == Display._mainDisplay.systemWidth))
+        if (!resDatas.Exists(a => a.m_Height == Display._mainDisplay.systemHeight && a.m_Width == Display._mainDisplay.systemWidth))
         {
             resDatas.Add(myResData);
-            Plugin.Logger.LogInfo($"Added {myResData.m_Width}x{myResData.m_Height} to resolution list");  
+            Plugin.Logger.LogInfo($"Added {myResData.m_Width}x{myResData.m_Height} to resolution list");
         }
 
         UI_GameplayOptions.ResolutionDatas = resDatas.ToArray();
--- Cuisineer/Cuisineer/Plugin.cs
+++ Cuisineer/Cuisineer/Plugin.cs
@@ -16,13 +16,18 @@ public class Plugin : BasePlugin
 {
     private const string PluginGuid = "p1xel8ted.cuisineer.cuisineertweaks";
     private const string PluginName = "Cuisineer Tweaks (IL2CPP)";
-    private const string PluginVersion = "0.1.0";
+    private const string PluginVersion = "0.1.1";
     internal static ManualLogSource Logger { get; private set; }
     private static ConfigEntry<bool> CorrectFixedUpdateRate { get; set; }
     private static ConfigEntry<bool> UseRefreshRateForFixedUpdateRate { get; set; }
+    internal static ConfigEntry<bool> IncreaseStackSize { get; private set; }
+    internal static ConfigEntry<bool> InstantText { get; private set; }
+    private static ConfigEntry<bool> EnableAutoSave { get; set; }
+    private static ConfigEntry<int> AutoSaveFrequency { get; set; }
     private static int MaxRefresh => Screen.resolutions.Max(a => a.refreshRate);
 
     private static int TimeScale => FindLowestFrameRateMultipleAboveFifty(MaxRefresh);
+
     private static int FindLowestFrameRateMultipleAboveFifty(int originalRate)
     {
         // Start from half of the original rate and decrement by one to find the highest multiple above 50.
@@ -41,17 +46,24 @@ public class Plugin : BasePlugin
     public override void Load()
     {
         Logger = Log;
-        CorrectFixedUpdateRate = Config.Bind("General", "CorrectFixedUpdateRate", true,
-            new ConfigDescription("Correct the fixed update rate to the lowest multiple of the refresh rate above 50.",
-                null));
-        UseRefreshRateForFixedUpdateRate = Config.Bind("General", "UseRefreshRateForFixedUpdateRate", true,
-            new ConfigDescription("Use the refresh rate for the fixed update rate. If false, the fixed update rate will be lowest multiple of the refresh rate above 50.",
-                null));
-
+        CorrectFixedUpdateRate = Config.Bind("01. General", "CorrectFixedUpdateRate", true,
+            new ConfigDescription("Correct the fixed update rate to the lowest multiple of the refresh rate above 50."));
+        UseRefreshRateForFixedUpdateRate = Config.Bind("01. General", "UseRefreshRateForFixedUpdateRate", true,
+            new ConfigDescription("Use the refresh rate for the fixed update rate. If false, the fixed update rate will be lowest multiple of the refresh rate above 50."));
+        InstantText = Config.Bind("01. General", "InstantText", true,
+            new ConfigDescription("Instantly show all text in cutscenes."));
+        IncreaseStackSize = Config.Bind("01. General", "IncreaseStackSize", true,
+            new ConfigDescription("Increase stack size to 999."));
+        EnableAutoSave = Config.Bind("01. General", "EnableAutoSave", true,
+            new ConfigDescription("Enable auto save."));
+        EnableAutoSave.SettingChanged += (_, _) => UpdateAutoSave();
+        AutoSaveFrequency = Config.Bind("01. General", "AutoSaveFrequency", 300, new ConfigDescription("Auto save frequency in seconds.", new AcceptableValueRange<int>(30, 600)));
+        AutoSaveFrequency.SettingChanged += (_, _) => UpdateAutoSave();
         SceneManager.sceneLoaded += (UnityAction<Scene, LoadSceneMode>) OnSceneLoaded;
 
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
         Logger.LogInfo($"Plugin {PluginGuid} is loaded!");
+        AddComponent<UnityEvents>();
     }
 
     private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -67,5 +79,48 @@ public class Plugin : BasePlugin
         var scale = UseRefreshRateForFixedUpdateRate.Value ? MaxRefresh : TimeScale;
         Time.fixedDeltaTime = 1f / scale;
         Logger.LogInfo($"Set fixedDeltaTime to {Time.fixedDeltaTime} ({scale}).");
+
+        UpdateAutoSave();
+        UpdateInventoryStackSize();
+    }
+
+    private static void UpdateInventoryStackSize()
+    {
+        if (InventoryManager.Instance == null) return;
+        foreach (var instanceMInventory in InventoryManager.Instance.m_Inventories)
+        {
+            if (instanceMInventory is {Value: null}) continue;
+            foreach (var valueMSlot in instanceMInventory.Value.m_Slots)
+            {
+                if (valueMSlot == null || valueMSlot.ItemSO == null) continue;
+                valueMSlot.ItemSO.m_MaxStack = 999;
+                Logger.LogInfo($"Set {valueMSlot.ItemID} stack size to {valueMSlot.ItemSO.m_MaxStack} in inventory {instanceMInventory.Value.Type}");
+            }
+        }
+    }
+
+    private static void UpdateAutoSave()
+    {
+        if (CuisineerSaveManager.Instance == null) return;
+        CuisineerSaveManager.Instance.m_AutoSave = EnableAutoSave.Value;
+        CuisineerSaveManager.Instance.m_AutoSaveFrequency = AutoSaveFrequency.Value;
+        Logger.LogInfo($"AutoSave: {CuisineerSaveManager.Instance.m_AutoSave} ({CuisineerSaveManager.Instance.m_AutoSaveFrequency / 60f} minutes)");
+    }
+
+    public class UnityEvents : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("UnityEvents Awake");
+        }
+
+        private void Update()
+        {
+            if (CuisineerInputWrapper.GetGameActionKeyUp(BattlebrewGameAction.DebugK))
+            {
+                CuisineerSaveManager.SaveCurrent();
+                Logger.LogInfo("Saved current game.");
+            }
+        }
     }
 }
