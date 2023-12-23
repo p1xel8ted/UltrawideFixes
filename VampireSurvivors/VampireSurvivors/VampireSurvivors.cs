b4a753f2b0b0a55cd59821585ef28b1e0f6d90da
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-12-23 16:34:01 +1100

    Vampire Survivors 0.0.5

--- VampireSurvivors/VampireSurvivors/VampireSurvivors.cs
+++ VampireSurvivors/VampireSurvivors/VampireSurvivors.cs
@@ -1,100 +1,82 @@
-﻿using Il2Cpp;
-using Il2CppInterop.Runtime.InteropTypes.Arrays;
-using Il2CppVampireSurvivors.UI;
-using MelonLoader;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Reflection;
+using Il2CppCom.LuisPedroFonseca.ProCamera2D;
 
-[assembly:
-    MelonInfo(typeof(VampireSurvivors.VampireSurvivors), "Vampire Survivors UltraWide Fixes", "0.0.4", "p1xel8ted")]
-[assembly: MelonGame("poncle", "Vampire Survivors")]
+[assembly: MelonInfo(typeof(VampireSurvivors.VampireSurvivors), "Vampire Survivors UltraWide Fixes", "0.0.5", "p1xel8ted")]
 
 namespace VampireSurvivors;
 
 public class VampireSurvivors : MelonMod
 {
-    private Il2CppArrayBase<ViewportCrop> _viewportCrop;
-    private Il2CppArrayBase<Camera> _cameras;
-    private Il2CppArrayBase<AspectRatioFitter> _aspectRatioFitters;
-    private Il2CppArrayBase<AspectMask> _aspectMasks;
+    private List<ViewportCrop> ViewportCrops { get; set; } = [];
+    private List<AspectMask> AspectMasks { get; set; } = [];
+    private static float UltraWidth => Display.main.systemWidth;
+    private static float Height => Display.main.systemHeight;
+    private static int MaxRefresh => Screen.resolutions.Max(a => a.refreshRate);
 
     public override void OnInitializeMelon()
     {
         base.OnInitializeMelon();
-
-        _cameras = Resources.FindObjectsOfTypeAll<Camera>();
-        _viewportCrop = Resources.FindObjectsOfTypeAll<ViewportCrop>();
-        _aspectRatioFitters = Resources.FindObjectsOfTypeAll<AspectRatioFitter>();
-        _aspectMasks = Resources.FindObjectsOfTypeAll<AspectMask>();
-
-        MelonLogger.Warning($"OnInitializeMelon: Found {_cameras.Length} Cameras");
-        MelonLogger.Warning($"OnInitializeMelon: Found {_viewportCrop.Length} Viewport Crops");
-        MelonLogger.Warning($"OnInitializeMelon: Found {_aspectRatioFitters.Length} Aspect Ratio Fitters");
-        MelonLogger.Warning($"OnInitializeMelon: Found {_aspectMasks.Length} Aspect Masks");
-
+        RefreshResources();
         MelonLogger.Msg("Vampire Survivors UltraWide Fixes Loaded");
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
-
-        foreach (var aspectMask in _aspectMasks.Where(a => a != null))
-        {
-            if (aspectMask.gameObject.activeSelf)
-            {
-                aspectMask.gameObject.SetActive(false);
-            }
-        }
-
-        foreach (var viewportCrop in _viewportCrop.Where(a => a != null))
-        {
-            viewportCrop._currentAspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
-            viewportCrop._referenceResolution = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
-            viewportCrop.ScreenRes = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
-        }
-
+        UpdateViewportAndAspectMasks();
     }
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
         base.OnSceneWasLoaded(buildIndex, sceneName);
+        RefreshResources();
+        UpdateResolution();
+        UpdateViewportAndAspectMasks();
+        UpdateBackgroundScaling();
+    }
+    private static void UpdateResolution()
+    {
+        Screen.SetResolution((int) UltraWidth, (int) Height, FullScreenMode.FullScreenWindow, MaxRefresh);
+        Application.targetFrameRate = MaxRefresh;
+        Time.fixedDeltaTime = 1f / MaxRefresh;
+    }
 
-        _cameras = Resources.FindObjectsOfTypeAll<Camera>();
-        _viewportCrop = Resources.FindObjectsOfTypeAll<ViewportCrop>();
-        _aspectRatioFitters = Resources.FindObjectsOfTypeAll<AspectRatioFitter>();
-        _aspectMasks = Resources.FindObjectsOfTypeAll<AspectMask>();
+    private void RefreshResources()
+    {
+        ViewportCrops = Utils.FindIl2CppType<ViewportCrop>();
+        AspectMasks = Utils.FindIl2CppType<AspectMask>();
+    }
+
+    private void UpdateViewportAndAspectMasks()
+    {
         
-        MelonLogger.Warning($"OnSceneWasLoaded: Found {_cameras.Length} Cameras");
-        MelonLogger.Warning($"OnSceneWasLoaded: Found {_viewportCrop.Length} Viewport Crop");
-        MelonLogger.Warning($"OnSceneWasLoaded: Found {_aspectRatioFitters.Length} Aspect Ratio Fitters");
-        MelonLogger.Warning($"OnSceneWasLoaded: Found {_aspectMasks.Length} Aspect Masks");
-
-        foreach (var aspectMask in _aspectMasks.Where(a => a != null))
+        var systemResolution = new Vector2(UltraWidth, Height);
+        
+        foreach (var aspectMask in AspectMasks.Where(a => a != null))
         {
-            if (aspectMask.gameObject.activeSelf)
-            {
-                aspectMask.gameObject.SetActive(false);
-            }
+            aspectMask.gameObject.SetActive(false);
         }
 
-        foreach (var viewportCrop in _viewportCrop.Where(a => a != null))
+        foreach (var viewportCrop in ViewportCrops.Where(a => a != null))
         {
-            viewportCrop._currentAspectRatio = Display.main.systemWidth / (float) Display.main.systemHeight;
-            viewportCrop._referenceResolution = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
-            viewportCrop.ScreenRes = new Vector2(Display.main.systemWidth, Display.main.systemHeight);
+            viewportCrop._currentAspectRatio = UltraWidth / Height;
+            viewportCrop._referenceResolution = systemResolution;
+            viewportCrop.ScreenRes = systemResolution;
         }
+    }
+    
+    private static void UpdateBackgroundScaling()
+    {
+        ScaleGameObject("GAME UI/Canvas - Game UI/Safe Area/View - Paused/Fade", 10f);
+        ScaleGameObject("GAME UI/Canvas - Game UI/Safe Area/View - ArcanaMainSelection/BlackFader", 10f);
+    }
 
-        var backgroundFade = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - Paused/Fade");
-        if (backgroundFade != null)
+    private static void ScaleGameObject(string path, float scaleX)
+    {
+        var gameObject = GameObject.Find(path);
+        if (gameObject != null)
         {
-            backgroundFade.transform.localScale = new Vector3(5f, 1, 1);
-        }
-
-        var backFade = GameObject.Find("GAME UI/Canvas - Game UI/Safe Area/View - ArcanaMainSelection/BlackFader");
-        if (backFade != null)
-        {
-            backFade.transform.localScale = new Vector3(5f, 1, 1);
+            gameObject.transform.localScale = new Vector3(scaleX, 1, 1);
         }
     }
 }
