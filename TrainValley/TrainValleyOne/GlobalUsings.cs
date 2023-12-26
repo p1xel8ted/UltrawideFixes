f81e620c5296476998418cc39080a2c30192a450
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-12-26 21:38:10 +1100

    Train Valley (1) 0.1.2

---
+++ TrainValley/TrainValleyOne/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Global using directives
+
+global using System.Reflection;
+global using BepInEx;
+global using BepInEx.Configuration;
+global using BepInEx.Logging;
+global using HarmonyLib;
+global using UnityEngine;
+global using UnityEngine.SceneManagement;
