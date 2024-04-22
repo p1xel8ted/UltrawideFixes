0606d6d3e4be76722b33fd56a2636d44e6ce5baf
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-04-22 21:11:07 +1000

    Smushi 0.1.2
    Smushi 0.1.2

--- src/SmushiComeHome/SmushiComeHome/Patches.cs
+++ src/SmushiComeHome/SmushiComeHome/Patches.cs
@@ -1,46 +1,96 @@
-﻿using System.Linq;
+﻿using Cinemachine;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SmushiComeHome;
 
 [Harmony]
 public static class Patches
 {
-    private static readonly ResItem ResItem = new()
+
+    private const string CmVcamPlayer = "CM VCAM Player";
+    private const string PixelCanvas = "Pixel Canvas";
+    private const string ComSinai = "com.sinai";
+
+    private readonly static ResItem ResItem = new()
     {
-        horizontal = Display.main.systemWidth,
-        vertical = Display.main.systemHeight
+        horizontal = Plugin.MainWidth,
+        vertical = Plugin.MainHeight
     };
-    
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(Camera), nameof(Camera.aspect), MethodType.Getter)]
-    public static bool Camera_aspect(ref float __result)
+
+    private readonly static WriteOnce<float> OriginalFoV = new();
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CinemachineVirtualCamera), nameof(CinemachineVirtualCamera.OnEnable))]
+    public static void CinemachineVirtualCamera_OnEnable(ref CinemachineVirtualCamera __instance)
     {
-        __result = (float) Display.main.systemWidth / Display.main.systemHeight;
-        return false;
+        if (__instance is not {Name: CmVcamPlayer}) return;
+        OriginalFoV.Value = __instance.m_Lens.FieldOfView;
     }
-    
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CinemachineVirtualCameraBase), nameof(CinemachineVirtualCameraBase.Update))]
+    public static void CinemachineVirtualCameraBase_Update(ref CinemachineVirtualCameraBase __instance)
+    {
+        if (__instance is not CinemachineVirtualCamera {Name: CmVcamPlayer} vcam) return;
+
+        if (Plugin.CustomFieldOfView.Value)
+        {
+            vcam.m_Lens.FieldOfView = Plugin.CustomFieldOfViewValue.Value;
+        }
+        else
+        {
+            vcam.m_Lens.FieldOfView = OriginalFoV.Value;
+        }
+    }
+
+    private static float GetNewScale(float reference)
+    {
+        var scale = 1f / (reference / Plugin.MainHeight);
+        return scale;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.OnEnable))]
+    public static void CanvasScaler_OnEnable(ref CanvasScaler __instance)
+    {
+        if (__instance.name.Equals(PixelCanvas) || __instance.name.Contains(ComSinai)) return;
+        __instance.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+        __instance.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.Handle))]
+    public static void CanvasScaler_Handle(ref CanvasScaler __instance)
+    {
+        if (__instance.name.Equals(PixelCanvas) || __instance.name.Contains(ComSinai)) return;
+        var newScale = GetNewScale(__instance.referenceResolution.y);
+        __instance.scaleFactor = Plugin.CustomScale.Value ? newScale * Plugin.CustomScaleValue.Value : newScale;
+    }
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(SaveSystem), nameof(SaveSystem.LoadSettingsData))]
     public static void SaveSystem_LoadSettingsData(ref SaveSystem __instance)
     {
-        __instance._gameSettings.resolutions.Clear();
-        __instance._gameSettings.resolutions.Add(ResItem);
-        __instance._gameSettings.resolutions.Add(ResItem);
+        var resolutions = __instance._gameSettings.resolutions;
+        resolutions.Clear();
+        resolutions.Add(ResItem);
+        resolutions.Add(ResItem);
     }
-    
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(SettingsConfigMainMenu), nameof(SettingsConfigMainMenu.GetScreenResolution))]
     public static bool SettingsConfigMainMenu_GetScreenResolution(ref SettingsConfigMainMenu __instance)
     {
-        Screen.SetResolution(Display.main.systemWidth, Display.main.systemHeight, FullScreenMode.FullScreenWindow, Screen.resolutions.Max(a => a.refreshRate));
-        SaveSystem.GetGameSettings().resolutions.Clear();
-        SaveSystem.GetGameSettings().resolutions.Add(ResItem);
-        SaveSystem.GetGameSettings().resolutions.Add(ResItem);
+        Screen.SetResolution(Plugin.MainWidth, Plugin.MainHeight, FullScreenMode.FullScreenWindow, Plugin.MaxRefreshRate);
+        var resolutions = SaveSystem.GetGameSettings().resolutions;
+        resolutions.Clear();
+        resolutions.Add(ResItem);
+        resolutions.Add(ResItem);
         return false;
     }
-    
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(SettingsConfigMainMenu), nameof(SettingsConfigMainMenu.SetFullScreen))]
     [HarmonyPatch(typeof(SettingsMenu), nameof(SettingsMenu.SetFullScreen))]
@@ -48,6 +98,6 @@ public static class Patches
     [HarmonyPatch(typeof(SettingsMenu), nameof(SettingsMenu.ChangeResolution))]
     public static void Settings_ChangeResolution()
     {
-        Screen.SetResolution(Display.main.systemWidth, Display.main.systemHeight, FullScreenMode.FullScreenWindow, Screen.resolutions.Max(a => a.refreshRate));
+        Screen.SetResolution(Plugin.MainWidth, Plugin.MainHeight, FullScreenMode.FullScreenWindow, Plugin.MaxRefreshRate);
     }
 }
