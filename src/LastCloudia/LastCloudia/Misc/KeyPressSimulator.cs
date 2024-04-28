5716f23d180a832f3d6dc4dc22c61323e3ea0ee6
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-04-28 16:20:34 +1000

    Last Cloudia 0.1.0

---
+++ src/LastCloudia/LastCloudia/Misc/KeyPressSimulator.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LastCloudia.Misc;
+
+internal static class KeyPressSimulator
+{
+    [DllImport("user32.dll")]
+    private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+    private const byte VK_ESCAPE = 0x1B;
+
+    internal static void PressESC()
+    {
+        keybd_event(VK_ESCAPE, 0, 0, 0);
+    }
+}
---
+++ src/LastCloudia/LastCloudia/MonoBehaviours/BgMaskScaler.cs
@@ -0,0 +1,10 @@
+﻿namespace LastCloudia.MonoBehaviours;
+
+public class BgMaskScaler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        transform.localScale = new Vector3(Plugin.PositiveScaleFactor * 2f, Plugin.PositiveScaleFactor * 2f, 1f);
+    }
+
+}
