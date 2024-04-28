e8255977ac0b6b88f866af06fd7e00c3a786baba
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-04-28 16:20:16 +1000

    NEO - The World Ends With You 0.1.0

---
+++ src/NEO-TheWorldEndsWithYou/NEO-TheWorldEndsWithYou/Patches.cs
@@ -0,0 +1,24 @@
+﻿namespace NEOTheWorldEndsWithYouUltraWide;
+
+[Harmony]
+[HarmonyWrapSafe]
+public static class Patches
+{
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.OnEnable))]
+    public static void CanvasScaler_OnEnable(ref CanvasScaler __instance)
+    {
+        __instance.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+    }
+
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GlobalBehaviour), nameof(GlobalBehaviour.CalcAspectAdjustSize16By9))]
+    [HarmonyPatch(typeof(GlobalBehaviour), nameof(GlobalBehaviour.CalcAspectAdjustSize))]
+    public static void GlobalBehaviour_CalcAspectAdjustSize(ref Vector2Int size, ref Vector2Int __result)
+    {
+        size = new Vector2Int(Plugin.MainWidth, Plugin.MainHeight);
+        __result = new Vector2Int(Plugin.MainWidth, Plugin.MainHeight);
+    }
+}
---
+++ src/NEO-TheWorldEndsWithYou/NEO-TheWorldEndsWithYou/Plugin.cs
@@ -0,0 +1,68 @@
+﻿namespace NEOTheWorldEndsWithYouUltraWide;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BasePlugin
+{
+    private const string PluginGuid = "p1xel8ted.neotheworldendswithyou.ultrawide";
+    private const string PluginName = "NEO - The World Ends With You Ultra-Wide";
+    private const string PluginVersion = "0.1.0";
+
+    internal static ConfigEntry<FullScreenMode> FullScreenModeConfig { get; private set; }
+    internal static int MainWidth => Display.displays[DisplayToUse.Value].systemWidth;
+    internal static int MainHeight => Display.displays[DisplayToUse.Value].systemHeight;
+    internal static int MaxRefresh => Screen.resolutions.Max(a => a.refreshRate);
+    private static ConfigEntry<bool> RunInBackground { get; set; }
+    private static ConfigEntry<bool> MuteInBackground { get; set; }
+    private static ConfigEntry<int> DisplayToUse { get; set; }
+    internal static ManualLogSource Logger { get; set; }
+
+    public override void Load()
+    {
+        Logger = Log;
+
+        FullScreenModeConfig = Config.Bind("01. Display", "Full Screen Mode", FullScreenMode.Windowed, new ConfigDescription("Set the full screen mode"));
+        FullScreenModeConfig.SettingChanged += (_, _) =>
+        {
+            UpdateDisplay();
+        };
+
+
+        DisplayToUse = Config.Bind("01. Display", "Display To Use", 0, new ConfigDescription("Display to use", new AcceptableValueList<int>(Display.displays.Select((_, i) => i).ToArray())));
+        DisplayToUse.SettingChanged += (_, _) =>
+        {
+            UpdateDisplay();
+        };
+
+        RunInBackground = Config.Bind("04. Misc", "Run In Background", true, new ConfigDescription("Allows the game to run even when not in focus.", null, new ConfigurationManagerAttributes {Order = 99}));
+        RunInBackground.SettingChanged += (_, _) =>
+        {
+            Application.runInBackground = RunInBackground.Value;
+        };
+
+        MuteInBackground = Config.Bind("04. Misc", "Mute In Background", false, new ConfigDescription("Mutes the game's audio when it is not in focus.", null, new ConfigurationManagerAttributes {Order = 98}));
+
+        Application.focusChanged += (Il2CppSystem.Action<bool>) FocusChanged;
+
+        SceneManager.sceneLoaded += (UnityAction<Scene, LoadSceneMode>) SceneManagerOnSceneLoaded;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
+        Log.LogInfo($"Plugin {PluginName} is loaded!");
+    }
+
+    private static void FocusChanged(bool focus)
+    {
+        AudioListener.pause = !focus && MuteInBackground.Value;
+    }
+
+    private static void SceneManagerOnSceneLoaded(Scene a, LoadSceneMode l)
+    {
+        UpdateDisplay();
+    }
+
+    private static void UpdateDisplay()
+    {
+        Display.displays[DisplayToUse.Value].Activate();
+        Screen.SetResolution(MainWidth, MainHeight, FullScreenMode.FullScreenWindow, MaxRefresh);
+        Application.targetFrameRate = MaxRefresh;
+    }
+
+}
