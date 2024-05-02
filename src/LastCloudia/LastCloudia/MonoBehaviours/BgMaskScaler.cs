9a1c233eb7326e63e834372c5e16a7ee5eec562d
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-05-02 20:08:50 +1000

    Last Cloudia 0.1.1

--- src/LastCloudia/LastCloudia/MonoBehaviours/BgMaskScaler.cs
+++ src/LastCloudia/LastCloudia/MonoBehaviours/BgMaskScaler.cs
@@ -2,6 +2,12 @@
 
 public class BgMaskScaler : MonoBehaviour
 {
+    
+    private void Start()
+    {
+        transform.localScale = new Vector3(Plugin.PositiveScaleFactor * 2f, Plugin.PositiveScaleFactor * 2f, 1f);
+    }
+    
     private void OnEnable()
     {
         transform.localScale = new Vector3(Plugin.PositiveScaleFactor * 2f, Plugin.PositiveScaleFactor * 2f, 1f);
