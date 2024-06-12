7fcc11a6b05e3901d1b5100ae4276410767957e8
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-06-12 20:54:11 +1000

    LIS BTS R

---
+++ src/LifeIsStrangeBTSR/LifeIsStrangeBTSR/Patches.cs
@@ -0,0 +1,123 @@
+﻿namespace LifeIsStrangeBTSR;
+
+[Harmony]
+public static class Patches
+{
+    private readonly static Dictionary<string, float> _cameraFovs = new();
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ButtonTweener), nameof(ButtonTweener.OnClick))]
+    public static void ButtonTweener_OnClick(ButtonTweener __instance)
+    {
+        if (__instance.name.Contains("apply", StringComparison.OrdinalIgnoreCase))
+        {
+            Plugin.UpdateDisplay();
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.ResetDefaults))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.RestorePriorToDefaultsValues))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.RevertResolutions))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.ApplyResolution))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.ApplyConfirmed))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.ApplyCanceled))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.ApplyPressed))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.BackCanceled))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.BackConfirmed))]
+    [HarmonyPatch(typeof(AdvancedVideoUI.RenderSettings), nameof(AdvancedVideoUI.RenderSettings.SetScreenResolution))]
+    public static void AdvancedVideoUI_RenderSettings_SetResolution()
+    {
+        Plugin.UpdateDisplay();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.SetVSyncOff))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.SetVSyncOn))]
+    [HarmonyPatch(typeof(DawnSettingsManager.RenderSettingsManager), nameof(DawnSettingsManager.RenderSettingsManager.SetVSync))]
+    [HarmonyPatch(typeof(DawnSettingsManager.RenderSettingsManager), nameof(DawnSettingsManager.RenderSettingsManager.SetVSyncNow))]
+    public static void AdvancedVideoUI_SetVSync(AdvancedVideoUI __instance)
+    {
+        Plugin.UpdateDisplay();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.Setup))]
+    [HarmonyPatch(typeof(AdvancedVideoUI), nameof(AdvancedVideoUI.LoadAdvancedSettings))]
+    public static void AdvancedVideoUI_Show(AdvancedVideoUI __instance)
+    {
+        __instance.m_VSyncUI.gameObject.SetActive(false);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SplashScreenWindow), nameof(SplashScreenWindow.Awake))]
+    [HarmonyPatch(typeof(SplashScreenWindow), nameof(SplashScreenWindow.GetSplashNext))]
+    [HarmonyPatch(typeof(SplashScreenWindow), nameof(SplashScreenWindow.StartSplash))]
+    public static void SplashScreenWindow_Patches(SplashScreenWindow __instance)
+    {
+        Plugin.UpdateDisplay();
+
+        __instance.m_continue = true;
+        __instance.m_fadeTime = 0;
+        __instance.m_transitionTime = 0;
+        __instance.m_splashScreenTime = 0;
+        __instance.m_waitStepTime = 0;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Camera), "set_fieldOfView")]
+    public static void PrefixSetFieldOfView(Camera __instance, ref float value)
+    {
+        if (__instance.name.Contains("MainMenu", StringComparison.OrdinalIgnoreCase)) return;
+        _cameraFovs[__instance.name] = value;
+        var pct = Plugin.FieldOfView.Value / 100f;
+        var newFov = value + value * pct;
+        value = newFov;
+    }
+
+    internal static void UpdateCamerasFoV()
+    {
+        var cameras = Utils.FindIl2CppType<Camera>();
+        foreach (var camera in cameras.Where(camera => !camera.name.Contains("MainMenu", StringComparison.OrdinalIgnoreCase)))
+        {
+            if (_cameraFovs.TryGetValue(camera.name, out var fov))
+            {
+                //we don't actually need to include the pct calcs, as the prefix patch will do it
+                camera.fieldOfView = fov;
+            }
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AdvancedVideoUI.RenderSettings), nameof(AdvancedVideoUI.RenderSettings.GenerateResolutions))]
+    public static void RenderSettings_GenerateResolutions(ref AdvancedVideoUI.RenderSettings __instance)
+    {
+        var myRes = new Resolution
+        {
+            width = Plugin.MainWidth,
+            height = Plugin.MainHeight,
+            refreshRate = Plugin.MaxRefresh
+        };
+
+        __instance.m_resolutions.Clear();
+        __instance.m_resolutions.Add(myRes);
+        __instance.m_resolutions.Add(myRes);
+        __instance.m_resolutions.Add(myRes);
+
+        __instance.m_resolutionsAsString.Clear();
+        __instance.m_resolutionsAsString.Add($"{myRes.width}x{myRes.height}");
+        __instance.m_resolutionsAsString.Add($"{myRes.width}x{myRes.height}");
+        __instance.m_resolutionsAsString.Add($"{myRes.width}x{myRes.height}");
+
+        __instance.m_currentResolution = myRes;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.OnEnable))]
+    public static void CanvasScaler_OnEnable(ref CanvasScaler __instance)
+    {
+        if (__instance.name.Contains("sinai", StringComparison.OrdinalIgnoreCase)) return;
+        __instance.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        __instance.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+    }
+}
---
+++ src/LifeIsStrangeBTSR/LifeIsStrangeBTSR/Plugin.cs
@@ -0,0 +1,111 @@
+﻿namespace LifeIsStrangeBTSR;
+
+[Harmony]
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BasePlugin
+{
+    private const string PluginGuid = "p1xel8ted.lifeisstrangebtsrm.ultrawide";
+    private const string PluginName = "Life is Strange: Before the Storm Remastered Ultra-Wide";
+    private const string PluginVersion = "0.1.0";
+
+    internal static ConfigEntry<int> DisplayToUse { get; private set; }
+    private static ConfigEntry<FullScreenMode> FullScreenModeConfig { get; set; }
+    internal static int MainWidth => Display.displays[DisplayToUse.Value].systemWidth;
+    internal static int MainHeight => Display.displays[DisplayToUse.Value].systemHeight;
+    private static WindowPositioner WindowPositioner { get; set; }
+    private static ConfigEntry<bool> AnisotropicFiltering { get; set; }
+    private static ConfigEntry<bool> RunInBackground { get; set; }
+    private static ConfigEntry<bool> MuteInBackground { get; set; }
+    private static ManualLogSource Logger { get; set; }
+    private static ConfigEntry<bool> Vignette { get; set; }
+    private static ConfigEntry<bool> Chromatic { get; set; }
+    internal static int MaxRefresh => Screen.resolutions.Max(a => a.refreshRate);
+    public static ConfigEntry<int> FieldOfView { get; private set; }
+    
+    public override void Load()
+    {
+        ClassInjector.RegisterTypeInIl2Cpp<WriteOnceFloat>();
+        Logger = Log;
+
+        FullScreenModeConfig = Config.Bind("01. Display", "Full Screen Mode", FullScreenMode.FullScreenWindow, new ConfigDescription("Set the full screen mode.", null, new ConfigurationManagerAttributes {Order = 17}));
+        FullScreenModeConfig.SettingChanged += (_, _) => UpdateDisplay();
+
+        DisplayToUse = Config.Bind("01. Display", "Display to Use", 0, new ConfigDescription("Select the display to use.", new AcceptableValueList<int>(Display.displays.Select((_, i) => i).ToArray()), new ConfigurationManagerAttributes {Order = 16}));
+        DisplayToUse.SettingChanged += (_, _) => UpdateDisplay();
+
+        if (PlatformHelper.Is(Platform.Windows) || PlatformHelper.Is(Platform.Wine))
+        {
+            WindowPositioner = AddComponent<WindowPositioner>();
+        }
+
+        FieldOfView = Config.Bind("02. Camera", "Field of View", 0, new ConfigDescription("Adjust the field of view of the camera by a percentage.", new AcceptableValueRange<int>(-75, 200), new ConfigurationManagerAttributes {Order = 15}));
+        FieldOfView.SettingChanged += (_, _) => UpdateFoV();
+
+        AnisotropicFiltering = Config.Bind("03. Quality", "Anisotropic Filtering", true, new ConfigDescription("Enable Anisotropic Filtering.", null, new ConfigurationManagerAttributes {Order = 12}));
+        AnisotropicFiltering.SettingChanged += (_, _) => QualitySettings.anisotropicFiltering = AnisotropicFiltering.Value ? UnityEngine.AnisotropicFiltering.ForceEnable : UnityEngine.AnisotropicFiltering.Disable;
+
+        Vignette = Config.Bind("04. Post Processing", "Vignette", false, new ConfigDescription("Enable Vignette.", null, new ConfigurationManagerAttributes {Order = 10}));
+        Vignette.SettingChanged += (_, _) => UpdatePostProcessing();
+
+        Chromatic = Config.Bind("04. Post Processing", "Chromatic Aberration", false, new ConfigDescription("Enable Chromatic Aberration.", null, new ConfigurationManagerAttributes {Order = 9}));
+        Chromatic.SettingChanged += (_, _) => UpdatePostProcessing();
+
+        RunInBackground = Config.Bind("05. Miscellaneous", "Run in Background", true, new ConfigDescription("Allow the game to run even when not in focus.", null, new ConfigurationManagerAttributes {Order = 2}));
+        RunInBackground.SettingChanged += (_, _) => Application.runInBackground = RunInBackground.Value;
+
+        MuteInBackground = Config.Bind("05. Miscellaneous", "Mute in Background", false, new ConfigDescription("Mute the game's audio when it is not in focus.", null, new ConfigurationManagerAttributes {Order = 1}));
+
+        Application.focusChanged += new Action<bool>(_ => AudioListener.pause = MuteInBackground.Value && !Application.isFocused);
+
+        SceneManager.sceneLoaded += (UnityAction<Scene, LoadSceneMode>) SceneManagerOnSceneLoaded;
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
+        Logger.LogInfo($"Plugin {PluginGuid} is loaded!");
+    }
+    internal static void UpdateDisplay()
+    {
+        if (WindowPositioner)
+        {
+            WindowPositioner.Start();
+        }
+        Application.runInBackground = RunInBackground.Value;
+        Display.displays[DisplayToUse.Value].Activate();
+        Screen.SetResolution(MainWidth, MainHeight, FullScreenModeConfig.Value, MaxRefresh);
+        Application.targetFrameRate = MaxRefresh;
+        UpdateCameraAspects();
+    }
+
+    private static void SceneManagerOnSceneLoaded(Scene a, LoadSceneMode l)
+    {
+        UpdateDisplay();
+        UpdateCameraAspects();
+        UpdateFoV();
+        UpdatePostProcessing();
+    }
+
+    private static void UpdateFoV()
+    {
+        Patches.UpdateCamerasFoV();
+    }
+
+    private static void UpdatePostProcessing()
+    {
+        var postProcessing = Utils.FindIl2CppType<PostProcessingBehaviour>();
+        foreach (var pp in postProcessing)
+        {
+            pp.profile.vignette.enabled = Vignette.Value;
+            pp.profile.chromaticAberration.enabled = Chromatic.Value;
+        }
+    }
+    private static void UpdateCameraAspects()
+    {
+        var cameras = Utils.FindIl2CppType<Camera>();
+        foreach (var cam in cameras)
+        {
+            cam.aspect = MainWidth / (float) MainHeight;
+            cam.rect = new Rect(0, 0, 1, 1);
+            cam.pixelRect = new Rect(0, 0, MainWidth, MainHeight);
+        }
+    }
+
+}
