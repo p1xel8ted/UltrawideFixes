f34cb9db829e484ad99b5845c43813ca05045e13
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-07-27 20:54:41 +1000

    Gestalt 0.1.1

--- src/GestaltSteamCinder/GestaltSteamCinder/Plugin.cs
+++ src/GestaltSteamCinder/GestaltSteamCinder/Plugin.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using BepInEx.Logging;
-using I2.Loc;
-using Il2CppInterop.Runtime;
-using JetBrains.Annotations;
-using PixelCrushers.DialogueSystem;
-using Object = UnityEngine.Object;
-
-namespace GestaltSteamCinder;
+﻿namespace GestaltSteamCinder;
 
 [HarmonyPatch]
-[HarmonyWrapSafe]
 [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
 public class Plugin : BasePlugin
 {
@@ -26,17 +17,14 @@ public class Plugin : BasePlugin
 
     private readonly static int MainWidth = Display.main.systemWidth;
     private readonly static int MainHeight = Display.main.systemHeight;
-    private readonly static int NativeWidth = Mathf.RoundToInt(MainHeight * NativeAspect);
-    private readonly static int BlackBarSize = (MainWidth - NativeWidth) / 2;
+    
     private readonly static float AspectRatio = MainWidth / (float) MainHeight;
     private readonly static float ScaleFactor = AspectRatio / NativeAspect;
     private readonly static int MaxRefreshRate = Screen.resolutions.Max(res => res.refreshRate);
-    private readonly static float MiddlePosition = MainWidth / 2f;
-    private readonly static float LeftLocation = MiddlePosition - BlackBarSize;
-    private readonly static float RightLocation = MiddlePosition + BlackBarSize;
-
+    
     private static float x;
     private static float y;
+    
 
     public override void Load()
     {
@@ -46,7 +34,6 @@ public class Plugin : BasePlugin
 
 
     [HarmonyPostfix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(SceneManager), nameof(SceneManager.Internal_SceneLoaded))]
     public static void SceneManager_Internal_SceneLoaded(Scene scene)
     {
@@ -59,24 +46,8 @@ public class Plugin : BasePlugin
         FixVignette();
 
         FixHeatShimmer();
-
-
-        DisableAnimators();
     }
-
-    private static void DisableAnimators()
-    {
-        var animators = Resources.FindObjectsOfTypeAll<Animator>().ToList();
-        var moreAnimators = Resources.FindObjectsOfTypeAll(Il2CppType.Of<Animator>());
-        animators.AddRange(moreAnimators.Select(ani => ani.TryCast<Animator>()).Where(a => a));
-
-
-        foreach (var ani in animators.Where(ani => ani.name.Contains("Portrait", StringComparison.OrdinalIgnoreCase)))
-        {
-            Debug.Log($"Disabling {ani.name} in {ani.gameObject.name}");
-            ani.enabled = false;
-        }
-    }
+    
 
     private static void SkipControllerMessage(Scene scene)
     {
@@ -131,35 +102,7 @@ public class Plugin : BasePlugin
         }
     }
 
-    // public static bool IsDecimal(double number)
-    // {
-    //     return number % 1 != 0;
-    // }
-    //
-    // private static void PrintSceneHierarchy()
-    // {
-    //     var sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-    //     foreach (var sceneObject in sceneObjects)
-    //     {
-    //         PrintObjectHierarchy(sceneObject.transform, sceneObject.name);
-    //     }
-    // }
-    //
-    // private static void PrintObjectHierarchy(Transform transform, string path)
-    // {
-    //     Debug.Log(path);
-    //     foreach (var o in transform)
-    //     {
-    //         var child = o.TryCast<Transform>();
-    //         if (child)
-    //         {
-    //             PrintObjectHierarchy(child, path + "/" + child.name);
-    //         }
-    //     }
-    // }
-
     [HarmonyPostfix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(ProCamera2D), nameof(ProCamera2D.LateUpdate))]
     public static void ProCamera2D_FixedUpdate(ProCamera2D __instance)
     {
@@ -180,100 +123,14 @@ public class Plugin : BasePlugin
 
 
     [HarmonyPostfix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.OnEnable))]
     public static void CanvasScaler_OnEnable(CanvasScaler __instance)
     {
         __instance.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
         __instance.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-
-        DisableAnimators();
-
-        // if (!__instance.name.Equals("DialogueCanvas")) return;
-        //
-        // var animators = __instance.GetComponentsInChildren<Animator>(true).ToList();
-        // animators.AddRange(__instance.GetComponents<Animator>());
-        //
-        // foreach (var ani in animators)
-        // {
-        //     if (ani.gameObject.name.Contains("Portrait", StringComparison.OrdinalIgnoreCase))
-        //     {
-        //         Debug.Log($"CanvasScaler: Disabling {ani.name} in {ani.gameObject.name}");
-        //         ani.enabled = false;
-        //     }
-        // }
     }
-    //
-    // [HarmonyPrefix]
-    // [HarmonyWrapSafe]
-    // [HarmonyPatch(typeof(CanvasScaler), nameof(CanvasScaler.Handle))]
-    // public static void CanvasScaler_Handle(CanvasScaler __instance)
-    // {
-    //     if (!__instance.name.Equals("DialogueCanvas")) return;
-    //
-    //     if (LeftPortraitsPanel)
-    //     {
-    //         var x1 = LeftPortraitsPanel.position.x;
-    //         if (!Mathf.Approximately(x1, LeftLocation))
-    //         {
-    //             LeftPortraitsPanel.position = LeftPortraitsPanel.position with {x = LeftLocation};
-    //         }
-    //     }
-    //
-    //     if (RightPortraitsPanel)
-    //     {
-    //         var x2 = RightPortraitsPanel.position.x;
-    //         if (!Mathf.Approximately(x2, RightLocation))
-    //         {
-    //             RightPortraitsPanel.position = RightPortraitsPanel.position with {x = RightLocation};
-    //         }
-    //     }
-    // }
-    //
-    // private static RectTransform LeftPortraitsPanel;
-    // private static RectTransform RightPortraitsPanel;
-
-    // [HarmonyPostfix]
-    // [HarmonyWrapSafe]
-    // [HarmonyPatch(typeof(GestaltVanguardStandardDialogueUI), nameof(GestaltVanguardStandardDialogueUI.OnConversationStart))]
-    // public static void GestaltVanguardStandardDialogueUI_OnConversationStart(GestaltVanguardStandardDialogueUI __instance)
-    // {
-    //     LeftPortraitsPanel = __instance.leftPortraitsPanel;
-    //     RightPortraitsPanel = __instance.rightPortraitsPanel;
-    //     var animators = __instance.GetComponentsInChildren<Animator>(true).ToList();
-    //     animators.AddRange(__instance.GetComponents<Animator>());
-    //
-    //     foreach (var ani in animators)
-    //     {
-    //         if (ani.gameObject.name.Contains("Portrait", StringComparison.OrdinalIgnoreCase))
-    //         {
-    //             ani.get_targetPosition_Injected(out var target);
-    //             {
-    //                 Debug.Log($"GestaltVanguardStandardDialogueUI: {ani.name} - {target}");
-    //             }
-    //
-    //             Debug.Log($"GestaltVanguardStandardDialogueUI: Disabling {ani.name} in {ani.gameObject.name}");
-    //             ani.enabled = false;
-    //         }
-    //     }
-    // }
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(Animator), nameof(Animator.SetTarget))]
-    public static void Animator_SetTarget(Animator __instance, AvatarTarget targetIndex, float targetNormalizedTime)
-    {
-        Debug.Log($"Animator: {__instance.name} - {targetIndex} - {targetNormalizedTime}");
-    }
-    
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(Animator), nameof(Animator.Update))]
-    public static void Animator_Update(Animator __instance)
-    {
-        Debug.Log($"Animator: {__instance.name}");
-    }
-
+   
     [HarmonyPrefix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(Screen), nameof(Screen.SetResolution), typeof(int), typeof(int), typeof(bool), typeof(int))]
     public static void Screen_SetResolution_One(ref int width, ref int height, ref bool fullscreen, ref int preferredRefreshRate)
     {
@@ -284,7 +141,6 @@ public class Plugin : BasePlugin
     }
 
     [HarmonyPrefix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(Screen), nameof(Screen.SetResolution), typeof(int), typeof(int), typeof(bool))]
     public static void Screen_SetResolution_Two(ref int width, ref int height, ref bool fullscreen)
     {
@@ -294,7 +150,6 @@ public class Plugin : BasePlugin
     }
 
     [HarmonyPrefix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(Screen), nameof(Screen.SetResolution), typeof(int), typeof(int), typeof(FullScreenMode))]
     public static void Screen_SetResolution_Three(ref int width, ref int height, ref FullScreenMode fullscreenMode)
     {
@@ -304,7 +159,6 @@ public class Plugin : BasePlugin
     }
 
     [HarmonyPrefix]
-    [HarmonyWrapSafe]
     [HarmonyPatch(typeof(Screen), nameof(Screen.SetResolution), typeof(int), typeof(int), typeof(FullScreenMode), typeof(int))]
     public static void Screen_SetResolution_Four(ref int width, ref int height, ref FullScreenMode fullscreenMode, ref int preferredRefreshRate)
     {
